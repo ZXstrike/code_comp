d11ad51d264c88060b3b1c32756a9440980aa70d
ZXstrikes <strike.miner0001@gmail.com>
2023-09-21 22:48:43 +0700

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# code_comp
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/code_comp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.code_comp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
